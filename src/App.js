6548f434b482e2a97af5bf8046f94ad1c56f65f2
dylan <dylanabozarth@gmail.com>
2020-11-25 20:50:48 -0800

    added features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
+import 'ag-grid-enterprise';
 import "./App.css";
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
@@ -9,23 +10,22 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 const App = () => {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
-
-    const [rowData, setRowData] = useState([
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Posdadsrsche", model: "Boxter", price: 72000},
-        {make: "Posad", model: "Boxter", price: 72000},
-        {make: "Porasdasdche", model: "Boxter", price: 72000}
-    ]);
+    const [rowData, setRowData] = useState([]);
+    // https://blockchain.info/ticker
+    useEffect(() => {
+            fetch('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/rowData.json')
+            .then(result => result.json())
+            .then(rowData => setRowData(rowData))
+         }, []);
 
     return (
         <div className="ag-theme-alpine-dark container " style={ { height: 400, width: 600 } }>
             <AgGridReact
+            rowSelection="multiple"
                 rowData={rowData}>
-                <AgGridColumn field="make"></AgGridColumn>
-                <AgGridColumn field="model"></AgGridColumn>
-                <AgGridColumn field="price"></AgGridColumn>
+                <AgGridColumn field="make"  filter={true} checkboxSelection={true} sortable={true}></AgGridColumn>
+                <AgGridColumn field="model" filter={true} checkboxSelection={true} sortable={true}></AgGridColumn>
+                <AgGridColumn field="price" filter={true} checkboxSelection={true} sortable={true}></AgGridColumn>
             </AgGridReact>
         </div>
     );
